410dc721aaadda68ee03bbebdbefc27a85f1c48e
Ubuntu <gabrieloest@nodejs-docker.m3ovnibreg0ephsykdfex4ujze.bx.internal.cloudapp.net>
2019-10-22 15:19:11 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// app.js
+
+const express = require('express')
+
+const app = express()
+
+const port = process.env.PORT
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get("/docker", (req, res) => {
+	res.send("Hello from Docker");
+});
+
+app.get('/nodemon', (req, res) => res.send('hello from nodemon'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
